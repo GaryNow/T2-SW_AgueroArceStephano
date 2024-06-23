211fe947f09ec50ea16755a9a18a33953c0fab05
Stephano <nafetsgary@outlook.es>
2024-06-23 11:05:36 -0500

    iniciando proyecto

---
+++ src/main/java/pe/edu/cibertec/t2sw_agueroarcestephano/T2SwAgueroArceStephanoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t2sw_agueroarcestephano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SwAgueroArceStephanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T2SwAgueroArceStephanoApplication.class, args);
+    }
+
+}
